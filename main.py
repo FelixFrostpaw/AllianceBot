960adcd761d80d024c0afbfc84d21e915d9af464
Felix Frostpaw <felixfrostpaw@gmail.com>
2023-07-14 22:37:18 -0700

    Add wrapper for GW2 API

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 
+import gw2api
+
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
 @bot.event
@@ -20,11 +22,13 @@ async def on_ready():
 async def version(interaction: discord.Interaction):
   await interaction.response.send_message(__version__, ephemeral=True)
 
-
 @bot.tree.command(name="register")
 @app_commands.describe(api_key="API Key")
 async def register(interaction: discord.Interaction, api_key: str):
   api_key = api_key.strip()
+
+  await gw2api.token_info(api_key)
+  
   message = f"{interaction.user.name} has registered with the API Key: '{api_key}'"
   await interaction.response.send_message(message, ephemeral=True)
 
